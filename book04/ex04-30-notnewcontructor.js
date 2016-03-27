484fd484d5978185bbf56eea97e216ed2f451628
soulduse <soulduse@naver.com>
2016-03-27 10:08:52 +0900

    no message

--- book04/ex04-30-notnewcontructor.js
+++ book04/ex04-30-notnewcontructor.js
@@ -38,3 +38,31 @@ console.log(a.value);
 console.log(b.value);
 console.log(global.value);
 
+console.log("----------------------------------------------------------------------------");
+
+
+function B(arg){
+	this.value = arg ? arg : 0;
+}
+
+var aa = new B(100);
+var bb = B(1000);
+console.log(aa.value);
+console.log(bb);
+console.log(global.value);
+
+function C(arg){
+	// console.log("arg Name >> "+arguments.callee);
+	// console.log("test");
+	if(!(this instanceof arguments.callee))
+		return new C(arg);
+	this.value = arg ? arg : 0;
+}
+
+console.log("test2");
+// 이렇게 입력하더라도 C() 함수에서 자동으로 new C 함수 객체를 리턴해줌. 
+//따라서 글로벌 변수에 value값이 할당되지 않는다.
+var c = C(10); 	
+console.log(c);
+console.log('cval : ' +c.value);
+console.log('gl : '+global.value);
