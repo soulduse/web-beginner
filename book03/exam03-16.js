8755537f14413ba804e42f807328fb68795c3259
soulduse <soulduse@naver.com>
2016-10-26 22:04:45 +0900

    array length 프로퍼티 공

--- book03/exam03-16.js
+++ book03/exam03-16.js
@@ -10,10 +10,11 @@ console.log(arr);			// [0, 1, 2, undefined , undefined ]
 arr.length = 2;
 console.log(arr.length);	// 2
 console.log(arr);			// [ 0, 1 ]
-console.log(arr[2]);
+console.log(arr[2]);		// undefined
 
 /*
 	length = 5를 줬을때 기존 값에서 벗어나는 경우 3,4 인덱스는
 	undefined로 출력된다. 하지만 반대로 length = 2를 줬을 때,
-
+	프로퍼티를 벗어나는 인덱스 값 2가 살제로 삭제된다.
+	따라서 console창에서는 undefined가 출력됨을 확인 할 수 있다.
 */
