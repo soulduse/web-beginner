d9d0c2acc9dce160ffd38e88a8b77776ea09f0ab
soulduse <soulduse@naver.com>
2016-10-27 20:23:20 +0900

    배열과 객체의 유사점과 차이점에 대해

--- book03/exam03-17.js
+++ book03/exam03-17.js
@@ -6,9 +6,14 @@ var arr = ['zero', 'one', 'two'];
 
 // push() 메서드 호출
 arr.push('three');
-console.log(arr);
+console.log(arr);	// [ 'zero', 'one', 'two', 'three' ]
 
 // length 값 변경 후, push() 메서드 호출
 arr.length = 5;
 arr.push('four');
-console.log(arr);
+console.log(arr);	// [ 'zero', 'one', 'two', ,'three' ]
+
+/*
+	위의 결과처럼 배열의 length 프로퍼티는 배열 메서드의 동작에
+	영향을 줄 수 있을만큼 배열에서는 중요한 프로퍼티 이므로 반드시 기억할 것.
+*/
