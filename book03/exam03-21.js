17446495272868c78132911bcac8c8e01ac22c68
soulduse <soulduse@naver.com>
2016-10-29 23:06:25 +0900

    배열 delete 연산 이후 길이 비

--- book03/exam03-21.js
+++ book03/exam03-21.js
@@ -16,13 +16,14 @@ console.log(arr.length);
 // 배열 객체 출력
 console.dir(arr);
 
-
 // 배열의 length프로퍼티는 배열 원소의 가장 큰 인덱스가 변했을 때만 변경됨.
 
+// for in을 사용하게 되면 0~3까지 배열 요소 포함, color, name 프로퍼티 까지 출력되었다.
 for(var prop in arr){
 	console.log(prop, arr[prop]);
 }
 
+// 정확히 배열의 요소만을 출력하고 있다.
 for(var i=0; i<arr.length; i++){
 	console.log(i, arr[i]);
 }
---
+++ book03/exam03-22.js
@@ -0,0 +1,6 @@
+// delete 연산자를 이용한 배열 프로퍼티 삭제
+
+var arr = ['zero', 'one', 'two', 'three'];
+
+// delete arr[2];
+console.log(arr.length);
