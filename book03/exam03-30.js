65cc5e614593182ebba0f12f0e28c27229bd3647
soulduse <soulduse@naver.com>
2016-11-01 12:22:39 +0900

    !! 연산자를 통한 불린값 변

---
+++ book03/exam03-30.js
@@ -0,0 +1,14 @@
+// !! 연산자 활용을 통한 불린값 변환
+
+console.log(!!0);			// false
+console.log(!!1);			// true
+console.log(!!'string');	// true
+console.log(!!'');			// false
+console.log(!!true);		// true
+console.log(!!false);		// false
+console.log(!!null);		// false
+console.log(!!undefined);	// false
+console.log(!!{});			// true
+console.log(!![1,2,3]);		// true
+
+console.log(null);
