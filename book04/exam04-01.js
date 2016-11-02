6bb36dcd3804f28f0101da88af4232eb920a8dc0
soulduse <soulduse@naver.com>
2016-11-02 21:46:14 +0900

    함수선언, 함수 표현식 으로 함수 생

---
+++ book04/exam04-01.js
@@ -0,0 +1,7 @@
+// add() 함수 생성 (함수 선언문 방식)
+
+function add(x,y){
+	return x + y;
+}
+
+console.log(add(3,4));	// 7
---
+++ book04/exam04-02.js
@@ -0,0 +1,33 @@
+/*
+	자바스크립트에서는 함수도 하나의 값처럼 취급된다
+	(이러한 특징으로 자바스크립트의 함수는 일급 객체라고 불린다.)
+	따라서 함수도 숫자나 문자열처럼 변수에 할당하는 것이 가능하다.
+
+	이러한 방식으로 함수 리터럴로 하나의 함수를 만들고, 여기서 생성된
+	함수를 변수에 할당하여 함수를 생성하는 것을 (함수 표현식) 이라고 한다.
+*/
+
+// add() 함수 생성 (함수 표현식 방식)
+
+var sum = function(x, y){
+	return x + y;
+};
+
+var injection = function(x, y){
+	var a = x;
+	var b = y;
+	var m = a*b;
+
+	var ddd = function(){
+		console.log('결과 : '+m);		// 25
+	}
+	ddd();
+
+	return sum(x,m);
+};
+
+var plus = sum;
+
+console.log(sum(3,4));			// 7
+console.log(plus(10,4));		// 14
+console.log(injection(5,5));	// 30
