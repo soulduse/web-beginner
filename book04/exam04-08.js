92a81e529d9947b0049654d8ff1c7bcb0db4a8fe
soulduse <soulduse@naver.com>
2016-11-09 19:51:15 +0900

    변수, 프로퍼티에 함수값 할당하기

--- book04/exam04-08.js
+++ book04/exam04-08.js
@@ -15,6 +15,15 @@ function add(x, y){
 add.result = add(3, 2);
 add.status = 'OK';
 
-console.log(add.result);
-console.log(add.status);
-console.log(add);
+console.log(add.result);	// 5
+console.log(add.status);	// 'OK'
+console.log(add);			// { [Function: add] result: 5, status: 'OK' }
+
+/* 
+	- add()함수를 생성할 때 함수 코드는 함수 객체의 [[Code]] 내부 프로퍼티에
+		자동으로 저장된다
+	- add()함수에 마치 일반객체처럼 result프로퍼티를 동적으로 생성하고,
+		여기에 add()함수를 호출한 결과를 저장한 것을 확인할 수 있다.
+		따라서 add(2,3) 함수 실행의 결과값인 5가 add()함수 객체의 result 프로퍼티에 저장된다.
+	- add() 함수 객체의 status 프로퍼티도 일반 객체에서 접근 방식처럼 add.satus를 이용해 접근 가능하다.
+*/
