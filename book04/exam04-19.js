1020f0146c20e9cbb8ff8553fcc00d1429b6475c
soulduse <soulduse@naver.com>
2016-12-10 09:47:44 -0600

    함수 스코프 외부에서 내부 함수 호출하기

---
+++ book04/exam04-19.js
@@ -0,0 +1,15 @@
+// 함수 스코프 외부에서 내부 함수 호출하는 예제
+
+function parent(){
+	var a = 100;
+	// child() 내부함수
+	var child = function(){
+		console.log(a);
+	}
+
+	// child() 함수 반환
+	return child;
+}
+
+var inner = parent();
+inner();
