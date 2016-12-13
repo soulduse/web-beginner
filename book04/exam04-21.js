27e7bbb95e629b34bfd0a2792636fdead366ee0e
soulduse <soulduse@naver.com>
2016-12-13 14:10:20 -0600

    arguments 객체 활용

--- book04/exam04-21.js
+++ book04/exam04-21.js
@@ -9,7 +9,15 @@ function func(arg1, arg2){
 	console.log(arg1, arg2);
 }
 
-func();
-func(1);
-func(1,2);
-func(1,2,3);
+func();			// undefined, undefined
+func(1);		// 1	undefined
+func(1,2);		// 1	2
+func(1,2,3);	// 1	2
+
+/*
+	결론은 정의된 함수의 인자보다 적게 함수 호출 
+	=> 넘겨지지 않은 인자에서는 undefined 값이 할당.
+
+	정의된 인자 개수보다 많게 함수를 호출
+	=> 에러 발생 x, 초과된 인수는 무시된다.
+*/
---
+++ book04/exam04-22.js
@@ -0,0 +1,35 @@
+// arguments 객체 예제 코드
+
+// add() 함수
+
+function add(a, b){
+	// arguments 객체 출력
+	console.dir(arguments);
+	return a+b;
+}
+
+console.log(add(1));		// NoN
+console.log(add(1,2));		// 3
+console.log(add(1,2,3));	// 3
+
+
+
+/*
+	arguments 객체는 매개변수 개수가 정확하게 정해지지 않은 함수를 구현하거나,
+	전달된 이낮의 개수에 따라 서로 다른 처리를 해줘야 하는 함수를 개발하는데
+	유용하게 사용 할 수 있다.	
+*/
+
+function sum(){
+	var result = 0;
+
+	for(var i=0; i<arguments.length; i++){
+		result += arguments[i];
+	}
+
+	return result;
+}
+
+console.log('============================');
+console.log(sum(1,2,3));
+console.log(sum(1,2,3,4,5,6,7,8,9));
