b64d071b9123a78217300ccefe87b7b334615b20
soulduse <soulduse@naver.com>
2016-12-24 15:39:54 -0600

    apply() 메서드 이용하여 명시적 this 바인딩

---
+++ book04/exam04-31.js
@@ -0,0 +1,15 @@
+// apply() 메서드 이용하여 명시적 this 바인딩
+
+// 생성자 함수
+function Person(name, age, gender){
+	this.name = name;
+	this.age = age;
+	this.gender = gender;
+}
+
+// foo 빈 객체 생성
+var foo = {};
+
+// apply() 메서드 호출
+Person.apply(foo, ['foo', 30, 'man']);
+console.dir(foo);
