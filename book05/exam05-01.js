aaa6e018593c7e34df25afd7931145357fdec95a
soulduse <soulduse@naver.com>
2017-01-06 00:13:30 -0600

    실행 컨텍스트

---
+++ book05/exam05-01.js
@@ -0,0 +1,14 @@
+console.log('This is global context');
+
+function ExContext1(){
+	console.log('This is ExContext1');
+};
+
+function ExContext2(){
+	ExContext1();
+	console.log('This is ExContext2');
+};
+
+ExContext2();
+
+// It is so easy for me.
