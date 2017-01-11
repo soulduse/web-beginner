fa8b3edf911eb1819452e047c90e5df7ac77684f
soulduse <soulduse@naver.com>
2017-01-10 22:31:17 -0600

    더글라스 크락포드 메서드 정의방법 응용 2

---
+++ book06/exam06-03.js
@@ -0,0 +1,23 @@
+Function.prototype.method = function(name, func){
+	if(!this.prototype[name]){
+		this.prototype[name] = func;
+	}
+}
+
+function Person(arg){
+	this.name = arg;
+}
+
+Person.method('setName', function(value){
+	this.name = value;
+});
+
+Person.method('getName', function(){
+	return this.name;
+});
+
+var me = new Person('me');
+var you = new Person('you');
+
+console.log(me.getName());
+console.log(you.getName());
