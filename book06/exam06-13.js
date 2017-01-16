66b6ab31c763460b9aeb51021948918e8534f02a
soulduse <soulduse@naver.com>
2017-01-15 23:24:14 -0600

    클래스 기능을 가진 subClass #2

--- book06/exam06-13.js
+++ book06/exam06-13.js
@@ -12,18 +12,41 @@
 
 
 function subClass(obj){
-
-	var parent = this;
+	var parent = this === global ? Function : this;
 	var F = function(){};
 
-	var child = function(){};
+	var child = function(){
+		var _parent = child.parent;
 
-	/* 프로토타입 체이닝 */
+		/*
+			현재 클래스의 부모 생성자가 있으며 그 함수를 호출한다. 다만
+			부모가 Function인 경우는 최상위 클래스에 도달했으므로
+			실행하지 않는다.
+		*/
+		if(_parent && _parent !== Function){
+			_parent.apply(this, arguments);	// 부모 함수의 재귀적 호출
+		}
+
+		if(child.prototype._init){
+			child.prototype._init.apply(this, arguments);
+		}
+	};
+
 	F.prototype = parent.prototype;
 	child.prototype = new F();
 	child.prototype.constructor = child;
-	child.parent = parent.prototype;
-	child.parent_constructor = parent;
+	child.parent = parent;
+	child.subClass = arguments.callee;
+	/*
+		arguments.callee는 현재 호출된 함수를 의미, 현재 호출된 함수가 
+		subClass이므로 child.subClass는 subClass 함수를 참조한다.
+	*/
+
+	for(var i in obj){
+		if(obj.hasOwnProperty(i)){
+			child.prototype[i] = obj[i];
+		}
+	}
 
 	return child;
 }
