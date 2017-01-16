d88cfbf06240b754994800360efbac39ee5e2dbc
soulduse <soulduse@naver.com>
2017-01-15 23:55:34 -0600

    예제 따라하기 - 상속편 [ 내용이해 50% ]

---
+++ book06/exam06-14.js
@@ -0,0 +1,76 @@
+function subClass(obj){
+	var parent = this === global ? Function : this;
+	var F = function(){};
+
+	var child = function(){
+		var _parent = child.parent;
+
+		/*
+			현재 클래스의 부모 생성자가 있으며 그 함수를 호출한다. 다만
+			부모가 Function인 경우는 최상위 클래스에 도달했으므로
+			실행하지 않는다.
+		*/
+		if(_parent && _parent !== Function){
+			_parent.apply(this, arguments);	// 부모 함수의 재귀적 호출
+		}
+
+		if(child.prototype._init){
+			child.prototype._init.apply(this, arguments);
+		}
+	};
+
+	F.prototype = parent.prototype;
+	child.prototype = new F();
+	child.prototype.constructor = child;
+	child.parent = parent;
+	child.subClass = arguments.callee;
+	/*
+		arguments.callee는 현재 호출된 함수를 의미, 현재 호출된 함수가 
+		subClass이므로 child.subClass는 subClass 함수를 참조한다.
+	*/
+
+	for(var i in obj){
+		if(obj.hasOwnProperty(i)){
+			child.prototype[i] = obj[i];
+		}
+	}
+
+	return child;
+}
+
+
+var person_obj = {
+	_init : function(){
+		console.log('person init');
+	},
+
+	getName : function(){
+		return this.name;
+	},
+	setName : function(name){
+		this.name = name;
+	}
+};
+
+var student_obj = {
+	_init : function(){
+		console.log('student init');
+	},
+	getName : function(){
+		return 'Student Name : '+this.name;
+	}
+};
+
+
+
+var Person = subClass(person_obj);	// Person 클래스 정의
+var person = new Person();			// person init 출력
+person.setName('dave');
+console.log(person.getName());		// 출력값 : dave
+
+var Student = Person.subClass(student_obj);	// Student 클래스 정의
+var student = new Student()			// person init, student init 출력
+student.setName('Eva');
+console.log(student.getName());
+
+// console.log(Person.toString());
