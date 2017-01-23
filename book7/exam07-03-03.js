09ca4423826aaa10013bcf68696692f60cbc4735
soulduse <soulduse@naver.com>
2017-01-22 23:40:15 -0600

    함수형 프로그래밍 + 클로저 이해할때 까지 도전하고 넘어가기.

--- book7/exam07-03-03.js
+++ book7/exam07-03-03.js
@@ -15,9 +15,44 @@ var fact = function(){
 	return func;
 }();
 
+
 console.log(fact(10));
 console.log(fact(20));
 
+
+/*
+	위에 소스 이해해보기
+*/
+
+var exam = function(){
+
+	var cache = {'0':1};
+
+	var func = function(n){
+		var result = 0;
+
+		console.log(typeof(cache[n]));
+		console.log(typeof('number'));
+		if(typeof(cache[n]) === 'number'){
+			result = cache[n];
+		}else{
+			result = cache[n] = n * func(n-1);
+		}
+		
+		console.log('----------------');
+		console.log(n);
+		console.log(result);
+		console.log('----------------');
+
+		return result;
+	}
+
+	return func;
+}();
+
+exam(5);
+
+/*
 function factA(num){
 
 	if(num == 0){
@@ -35,3 +70,5 @@ function factB(num){
 
 console.log(factA(3));
 console.log(factB(20));
+
+*/
