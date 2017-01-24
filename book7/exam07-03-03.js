a187385a8a3de5358ea68b04314cadb60e381e82
soulduse <soulduse@naver.com>
2017-01-23 23:19:48 -0600

    함수형 프로그래밍 코드이해하기 완료

--- book7/exam07-03-03.js
+++ book7/exam07-03-03.js
@@ -1,3 +1,4 @@
+/*
 var fact = function(){
 	var cache = {'0': 1};
 	var func = function(n){
@@ -18,10 +19,17 @@ var fact = function(){
 
 console.log(fact(10));
 console.log(fact(20));
-
+*/
 
 /*
-	위에 소스 이해해보기
+	위에 소스 이해해보기 : 이해 완료
+	설명 : 
+		exam function을 바로 실행시켜 cashe에 접근 할 수 있는
+		클로저를 반환받는다. 클로저로 숨겨지는 cache에는 팩토리얼을
+		연산한 값을 저장하고 있다. 연산을 수행하는 과정에서 캐시에
+		저장된 값이 있으면 곧바로 그 값을 반환하는 방식이다.
+		이렇게 하면 한번 연산된 값을 캐시에 저장하고 있으므로, 중복된
+		연산을 피하여 보다 나은 성능의 함수를 구현 할 수 있다.
 */
 
 var exam = function(){
@@ -29,6 +37,7 @@ var exam = function(){
 	var cache = {'0':1};
 
 	var func = function(n){
+		console.log('funcCnt : '+n);
 		var result = 0;
 
 		console.log(typeof(cache[n]));
@@ -38,6 +47,8 @@ var exam = function(){
 		}else{
 			result = cache[n] = n * func(n-1);
 		}
+
+		console.log('cacheNum : '+cache[n]);
 		
 		console.log('----------------');
 		console.log(n);
@@ -50,7 +61,34 @@ var exam = function(){
 	return func;
 }();
 
-exam(5);
+console.log(typeof(exam(5)));
+
+/*
+var ca = {'0':1};
+console.log(typeof(ca[0]) === 'number');
+*/
+
+/* 
+	아래와 같은 정의가 가능하다. ( 이건 몰랐네.. ) 
+	따라서 위의
+
+var a = 0;
+var b = 0;
+var c = 0;
+
+a = b = c = 5;
+console.log(a);		// 5
+console.log(b);		// 5
+console.log(c);		// 5
+
+a = b=  0;
+console.log(a);		// 0
+console.log(b);		// 0
+console.log(c);		// 5
+
+exam(3);
+
+*/
 
 /*
 function factA(num){
